555d514e452fd8a1387880bc7e405b37fa6353df
Rudy Gamberini <RudyGamberini@gmail.com>
2014-11-10 21:09:20 -0500

    Repo cleaning and Zombie attempt Big work on Zombie Attempt nearly finished now TODO: Split into multiple JS files maybe? Also tries to clean up all that repo fuckery from last committo multiple

--- ZombieAttempt/JS/main.js
+++ ZombieAttempt/JS/main.js
@@ -22,7 +22,11 @@ function shuffle(array) {
 }
 
 function entityAt(loc) {
-  return map[loc.x][loc.y];
+  if (inBounds(loc)) {
+    return map[loc.x][loc.y];
+  }else {
+    return null;
+  }
 }
 
 function Location(x, y) {
@@ -55,6 +59,14 @@ function moveLoc(loc, newLoc) {
   return true;
 }
 
+function getNeighbors(loc, dir) {
+  var neighbors = new Array(dir.length)
+  for (var i = 0; i < dir.length; i++) {
+    neighbors[i] = entityAt(loc.add(dir[i]));
+  }
+  return neighbors;
+}
+
 function inBounds(loc) {
   return loc.x >= 0 && loc.x < size && loc.y >= 0 && loc.y < size
 }
@@ -65,41 +77,57 @@ var types = [ //Where I keep the state info
 
     {
       name: "Empty",
-      id: 0
+      id: 0,
+      actions: []
     },
 
     {
       name: "Zombie",
       id: 1,
-      move: function(loc) {
+      actions:
+      [ function move(loc) {
+        shuffle(dirs); //List of random directions
         for (var i = 0; i < dirs.length; i++) {
           var newLoc = loc.add(dirVectors[dirs[i]]);
           if (canMove(newLoc)) {
-            shuffle(dirs); //List of random directions
             return moveLoc(loc, newLoc); // Do the move
           }
         }
         return false;
+      },
+      function bite(loc) {
+        shuffle(cardinaldirs);
+        var neighbors = getNeighbors(loc, cardinaldirs); //Remember to shuffle!
+        for (var i = 0; i < neighbors.length; i++) {
+          if (neighbors[i] == 2 || neighbors[i] == 3) {
+            neighbors[i] = 1;
+          }
+        }
       }
+       ]
     },
 
     {
       name: "Victim",
       id: 2,
-      move: function(loc) {
-        for (var i = 0; i < dirs.size; i++) {
-          if (entityAt(loc.add(cardinaldirs[i])) == 1) {
+      actions:
+      [ function move(loc) {
+        var neighbors = getNeighbors(loc, dirs);
+        for (var i = 0; i < neighbors.length; i++) {
+          if (neighbors[i] == 1) {
             return moveDir(loc, cardinaldirs[getRandomInt(0, 3)]);
           }
         }
         return false;
-      }
+      } ]
     },
 
     {
       name: "Hunter",
       id: 3,
-      move: function(loc) {
+      actions: [
+      function move(loc) {
+        shuffle(dirs);
         for (var i = 0; i < dirs.length; i++) {
           var newLoc = loc.add(dirVectors[dirs[i]]);
           if (canMove(newLoc)) {
@@ -108,7 +136,17 @@ var types = [ //Where I keep the state info
           }
         }
         return false;
-      }
+      },
+      function hunt(loc) {
+        var neighbors = getNeighbors(loc, dirs),
+        c = 0;
+        for (var i  = 0; i < neighbors.length; i++) {
+          if (neighbors[i] == 1) {
+            neighbors[i] = 0;
+          }
+          if (c == 2) {break;}
+        }
+      }]
     }
   ],
 
@@ -129,16 +167,17 @@ for (var i = 0; i < size; i++) { //Fill Map
   }
 }
 
-map = [[1,1,1], [2,0,2], [3,3,3]];
+map = [[1,0,0], [0,2,0], [0,0,1]];
 
 var z = 100,
   v = 100,
   h = 100,
-  t = 25; //Constants
+  t = 25, MAXACTIONS = 2; //Constants
 
 // for (i = 0; i < 10; i++) {
 //   gameloop();
 // }
+prettyPrint(map);
 gameloop();
 gameloop();
 
@@ -149,16 +188,21 @@ function prettyPrint(map) {
     }
     process.stdout.write("\n");
   }
+  process.stdout.write("\n");
 }
 
 function gameloop() {
-  prettyPrint(map);
-  for (var i = 0; i < size; i++) { //Fill Map
-    for (var j = 0; j < size; j++) {
-      if (map[i][j] !== 0) {
-        types[map[i][j]].move(new Location(i, j));
+  for (var currentAction = 0; currentAction < MAXACTIONS; currentAction++) {
+    for (var i = 0; i < size; i++) { //Fill Map
+      for (var j = 0; j < size; j++) {
+        if (map[i][j] !== 0) {
+          type = types[map[i][j]];
+          if (currentAction < type.actions.length) {
+            type.actions[currentAction](new Location(i, j));
+          }
+        }
       }
     }
-    console.log()
   }
+  prettyPrint(map);
 }
